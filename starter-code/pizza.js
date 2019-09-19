41ffb522c6c00c4cac9a97600acb08539e000732
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-19 15:42:34 -0300

    class dojo

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,18 @@
-// Write your Pizza Builder JavaScript in this file.
+function toggleElement(targetClassName, toggleClass) {
+  let elements = document.querySelectorAll(targetClassName);
+  elements.forEach((element) => {
+    element.classList.toggle(toggleClass);
+  });
+}
+
+let pepperButton = document.querySelector('.btn-green-peppers')
+pepperButton.onclick = () => toggleElement(".green-pepper", 'hidden');
+
+let pepperonniButton = document.querySelector('.btn-pepperonni')
+pepperonniButton.onclick = () => toggleElement(".pep", 'hidden');
+
+let mushroomButton = document.querySelector('.btn-mushrooms')
+mushroomButton.onclick = () => toggleElement(".mushroom", 'hidden');
+
+let crustButton = document.querySelector('.btn-crust')
+crustButton.onclick = () => toggleElement('.crust', 'crust-gluten-free');
